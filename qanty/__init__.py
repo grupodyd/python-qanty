f5e14a893a749e6e46fe3f5ae9bcf8f1e4aa8fcc
Juan F. Duque <jfduque@gmail.com>
2023-10-12 23:41:43 -0500

    Avances

--- qanty/__init__.py
+++ qanty/__init__.py
@@ -1,17 +1,18 @@
 # -*- coding: UTF-8 -*-
 import logging
-from typing import Optional, Union
+from typing import List, Optional
 
 import httpx
 
-import qanty.common.dataclasses
+import qanty.common.models as models
+
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
 class Qanty:
-    url = "https://qanty.com/api"
+    __url = "https://qanty.com/api"
 
     def __init__(self, auth_token: str, company_id: str) -> None:
         self.client = httpx.Client(http2=True)
@@ -21,13 +22,58 @@ class Qanty:
     def __del__(self) -> None:
         self.client.close()
 
-    def get_branches(self) -> Optional[list]:
-        url = f"{self.url}/company/get_branches"
+    def get_branches(self, filters: Optional[dict] = None, get_deleted: Optional[bool] = False) -> Optional[List[models.Branch]]:
+        """
+        Retrieves a list of branches for the company associated with this Qanty instance.
+
+        :param filters: A dictionary of filters to apply to the branch list. Optional.
+        :param get_deleted: Whether to include deleted branches in the list. Optional.
+        :return: A list of Branch objects representing the branches for the company, or None if an error occurred.
+        """
+        url = f"{self.__url}/company/get_branches"
         try:
-            response = self.client.post(url, json={"company_id": self.company_id})
+            response = self.client.post(url, json={"company_id": self.company_id, "filters": filters, "get_deleted": get_deleted})
             response.raise_for_status()
+            data = response.json()
         except httpx.HTTPStatusError as exc:
             logger.error(exc)
             return None
 
-        return response.text
+        sites = data.get("sites", [])
+        branches: List[models.Branch] = [models.Branch.model_validate(item) for item in sites]
+        return branches
+
+    def get_lines(
+        self, branch_id: Optional[str] = None, custom_branch_id: Optional[str] = None, get_deleted: Optional[bool] = False
+    ) -> Optional[List[models.Line]]:
+        """
+        Retrieves a list of lines from the specified branch or custom branch.
+
+        Args:
+            branch_id (Optional[str]): The ID of the branch to retrieve lines from. Defaults to None.
+            custom_branch_id (Optional[str]): The ID of the custom branch to retrieve lines from. Defaults to None.
+            get_deleted (Optional[bool]): Whether to include deleted lines in the results. Defaults to False.
+
+        Returns:
+            Optional[List[models.Line]]: A list of Line objects representing the retrieved lines, or None if an error occurred.
+        """
+        url = f"{self.__url}/branches/get_lines"
+        try:
+            response = self.client.post(
+                url,
+                json={
+                    "company_id": self.company_id,
+                    "branch_id": branch_id,
+                    "custom_branch_id": custom_branch_id,
+                    "get_deleted": get_deleted,
+                },
+            )
+            response.raise_for_status()
+            data = response.json()
+        except httpx.HTTPStatusError as exc:
+            logger.error(exc)
+            return None
+
+        lines = data.get("lines", [])
+        lines: List[models.Line] = [models.Line.model_validate(item) for item in lines]
+        return lines
--- tests/test_qanty.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-
-def test_get_branches(qanty):
-    response = qanty.get_branches()
-    assert response is not None
