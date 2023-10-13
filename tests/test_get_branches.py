a3c26f0533a96dd29ad043ccaff9a6b2b101f6d6
Juan F. Duque <jfduque@gmail.com>
2023-10-13 16:31:21 -0500

    Fix

--- tests/test_get_branches.py
+++ tests/test_get_branches.py
@@ -6,6 +6,9 @@ import qanty.common.models as models
 def test_get_branches(qanty):
     response = qanty.get_branches()
     assert isinstance(response, list)
+    if len(response) > 0:
+        for branch in response:
+            assert isinstance(branch, models.Branch)
 
 
 # def test_get_deleted_branches(qanty):
